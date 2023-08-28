e2ea089e671439138ca82bef9c02b25840ae9e85
FilippoMarletta <filippomarletta9@gmail.com>
2023-08-28 17:42:08 +0200

    Cronfunction funzionante

---
+++ E04/CronFunction/src/tests/unit/test-handler.mjs
@@ -0,0 +1,14 @@
+'use strict';
+
+import { lambdaHandler } from '../../app.mjs';
+import { expect } from 'chai';
+var event, context;
+
+describe('Tests index', function () {
+    it('verifies successful response', async () => {
+        const result = await lambdaHandler(event, context)
+
+        expect(result).to.be.an('object');
+    
+    });
+});
