d19a1c98b2323f96ecd77745a69b9321df5cded7
FilippoMarletta <filippomarletta9@gmail.com>
2023-08-29 12:42:58 +0200

    configurato Eslint

--- E04/CronFunction/src/app.mjs
+++ E04/CronFunction/src/app.mjs
@@ -3,21 +3,21 @@
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
  * @param {Object} event - API Gateway Lambda Proxy Input Format
  *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
+ * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html
  * @param {Object} context
  *
  * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
+ *
  */
 
 export const lambdaHandler = async (event, context) => {
-    try {
-        const dateObject = new Date();
-        console.log("Sone le " + dateObject.getHours() + ":" + dateObject.getMinutes() +  " e tutto tace ...");
-        return {};
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
-};
+  try {
+    const dateObject = new Date()
+    console.log('Sone le ' + dateObject.getHours() + ':' + dateObject.getMinutes() + ' e tutto tace ...')
+    return {}
+  } catch (err) {
+    console.log(err)
+    return err
+  }
+}
