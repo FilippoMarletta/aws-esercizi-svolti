e4871f74af4aa94352ae2be5ab0f22dbd8f22f9e
FilippoMarletta <filippomarletta9@gmail.com>
2023-09-04 15:16:23 +0200

    aggiunto E05, non funzionante

--- E05/ApigatewayToLambdaToDynamoDB/src/app.mjs
+++ E05/ApigatewayToLambdaToDynamoDB/src/app.mjs
@@ -1,3 +1,12 @@
+import { Logger } from '@aws-lambda-powertools/logger'
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocumentClient, UpdateCommand } from '@aws-sdk/lib-dynamodb'
+
+const logger = new Logger({ serviceName: 'serverlessDynamoDB', logLevel: 'DEBUG' })
+
+const ddbClient = new DynamoDBClient({})
+const ddbDocumentClient = new DynamoDBDocumentClient.from(ddbClient)
+
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -12,4 +21,38 @@
  */
 
 export const lambdaHandler = async (event, context) => {
+  logger.info(event)
+
+  const body = JSON.parse(event.body)
+
+  try {
+    const response = await ddbDocumentClient.send(new UpdateCommand({
+      TableName: process.env.DynamoDB_TABLE,
+      Key: {
+        FilmId: body.filmId
+      },
+      UpdateExpression: 'set Genere = :genere, Title = :title',
+      ExpressionAttributeValues: {
+        ':genere': body.genere,
+        ':title': body.title
+      },
+      ReturnValues: 'ALL_NEW'
+    }))
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(response)
+    }
+  } catch (err) {
+    logger.error(err)
+    return {
+      statusCode: 500,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(err)
+    }
+  }
 }
